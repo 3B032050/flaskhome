2a7f2915ba15a2ab9f5b808f3a0e2656021514c9
黃文輝 <antory040512@gmail.com>
2024-04-02 15:19:21 +0800

    修改

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def index():
 def hello():
     return '<h1>Hello Flask</h1>'
 @app.route('/user/<name>')
-def user():
+def user(name):
     return f'<h1>Hello,{name}!</h1>'
 @app.route('/user/<name>/<surname>')
-def surname():
+def surname(name,surname):
     return f'<h1>Hello,{name}{surname}!</h1>'
 if __name__ == '__main__':
     app.run()
