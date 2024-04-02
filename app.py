32eb602bcfbb9a391b8f9f3ba0a8051fa61dffda
黃文輝 <antory040512@gmail.com>
2024-04-02 15:14:27 +0800

    第三次提交

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello world</h1>'
+@app.route('/hello')
+def hello():
+    return '<h1>Hello Flask</h1>'
+@app.route('/user/<name>')
+def user():
+    return f'<h1>Hello,{name}!</h1>'
+@app.route('/user/<name>/<surname>')
+def surname():
+    return f'<h1>Hello,{name}{surname}!</h1>'
+if __name__ == '__main__':
+    app.run()
