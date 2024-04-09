8d7a98a1bb34230aed6e630f3709d1ba835aa41a
黃文輝 <antory040512@gmail.com>
2024-04-09 15:45:08 +0800

    修改

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask,rendor_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return rendor_template('index.html')
+    return render_template('index.html')
 @app.route('/hello')
 def hello():
     return '<h1>Hello Flask</h1>'
 @app.route('/user/<name>')
 def user(name):
-    return rendor_template('index.html',name=name)
+    return render_template('index.html',name=name)
 @app.route('/user/<name>/<surname>')
 def surname(name,surname):
     return f'<h1>Hello,{name}{surname}!</h1>'
