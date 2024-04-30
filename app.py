ef91fe3fcf5cc7298feb27aaacdc1a18205b5cd9
黃文輝 <antory040512@gmail.com>
2024-04-30 15:09:41 +0800

    修改

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template ,request
 
 app = Flask(__name__, template_folder='templates',
             static_url_path='/static', static_folder='static')
-
+name = ''
 @app.route('/')
 @app.route('/index',methods=['GET'])
 def index():
@@ -29,9 +29,34 @@ def ticket():
 def welfare():
     return render_template('welfare.html')
 
-@app.route('/user/<name>')
+'''@app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name)'''
+
+@app.route('/user/<username>')
+def user(username):
+    global name
+    name = username
+    return render_template('user.html', name=username)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    global name
+    username = name
+    if request.methods =='POST':
+        keyword = request.values['keyword']
+        if keyword == '紅燈':
+            message = '紅燈停!'
+        elif keyword == '黃燈':
+            message = '加速通過馬路或停下等候綠燈!'
+        elif keyword == '綠燈':
+            message = '綠燈行!'
+        else:
+            message = '請重新輸入!'
+        return render_template('user.html', name=username, message=message)
+    else:
+        message = '請使用HTTP POST傳送資料!'
+        return render_template('result.html', message=message)
 
 @app.route('/user/<name>/<surname>')
 def user_surname(name,surname):
