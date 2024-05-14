a994c30c983b145862a77f1fbd0a81371b8dda40
黃文輝 <antory040512@gmail.com>
2024-05-14 17:12:19 +0800

    修改

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 26 15:04:03 2024
@@ -7,11 +6,27 @@ Created on Tue Mar 26 15:04:03 2024
 """
 
 from flask import Flask, render_template ,request
-from flask import redirect, url_for
+from flask import redirect, url_for, session
+from datetime import timedelta
+import hashlib
+import psycopg2
+import dbconn
 
 app = Flask(__name__, template_folder='templates',
             static_url_path='/static', static_folder='static')
+app.secret_key = 'fd4723e200261a2271ea912571eaaald'
+app.permanent_session_lifetime = timedelta(minutes=3)
 name = ''
+
+# DB Connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=dbconn.host,
+        database=dbconn.database,
+        user=dbconn.user,
+        password=dbconn.password)
+    return conn
+
 @app.route('/')
 @app.route('/index',methods=['GET'])
 def index():
@@ -30,15 +45,23 @@ def ticket():
 def welfare():
     return render_template('welfare.html')
 
-@app.route('/user/<username>', methods=["GET"])
+'''@app.route('/user/<username>', methods=["GET"])
 def user(username):
     message= request.args.get("message")
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name)'''
+
+@app.route('/user')
+def user():
+    if 'username' in session:
+        username = session['username']
+        return render_template('user.html', name=username)
+    else:
+        return redirect(url_for('signin'))
 
 @app.route('/member/signin')
 def signin():
     return render_template('member/signin.html')
-@app.route('/member/login', methods=["POST"])
+'''@app.route('/member/login', methods=["POST"])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -46,8 +69,61 @@ def login():
         message = '登入成功!'
         return redirect(url_for('user', username=username, message=message))
     else:
-        render_template("member/signin.html")
+        render_template("member/signin.html")'''
+@app.route('/member/login', methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        userpass = request.form['userpassword']
+        md = hashlib.md5()
+        md.update(userpass.encode('utf-8'))
+        hashpass = md.hexdigest()
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        SQL = f"SELECT username, userpass FROM account WHERE username='{username}';"
+        cursor.execute (SQL)
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if not user:
+            return redirect(url_for('signin'))
 
+        if (username == user [0] and hashpass == user[1]):
+            session.parmanent = True
+            session['username'] = username
+            return redirect(url_for('user'))
+        else:
+            return redirect(url_for('signin'))
+    else:
+        if 'username' in session:
+            return redirect(url_for('user'))
+
+        return render_template("member/signin.html")
+'''@app.route('/member/login', methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        userpass = request.form['userpassword']
+        md = hashlib.md5()
+        md.update(userpass.encode('utf-8'))
+        hashpass = md.hexdigest()
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        SQL = f"SELECT username, userpass FROM account WHERE username='{username}';"
+        cursor.execute(SQL)
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if user and hashpass == user[1]:
+            session.permanent = True
+            session['username'] = username
+            return redirect(url_for('user'))
+        else:
+            return render_template("member/signin.html")
+    else:
+        if 'username' in session:
+            return redirect(url_for('user'))
+        return render_template("member/signin.html")'''
 '''@app.route('/user/<username>')
 def user(username):
     global name
@@ -56,8 +132,10 @@ def user(username):
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    global name
-    username = name
+    '''global name
+    username = name'''
+    if 'username' in session:
+        username = session['username']
     if request.method =='POST':
         keyword = request.values['keyword']
         if keyword == '紅燈':
---
+++ dbconn.py
@@ -0,0 +1,4 @@
+host = 'dpg-cojk7o8cmk4c73bst0n0-a.singapore-postgres.render.com'
+database = 'smarthome_4mts'
+user = 'admin'
+password = 'YUUHST814lZfRPLvHCUGdSE6cZfp1OfN'
